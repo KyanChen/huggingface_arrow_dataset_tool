7823296a098aeb8b2ce4946d46e96840e873c66c
Keyan Chen <keyan1055002755@163.com>
2024-09-22 15:40:25 +0800

    Update dataset_generator.py

--- dataset_generator.py
+++ dataset_generator.py
@@ -23,8 +23,8 @@ if __name__ == '__main__':
 
 	img_folder = '/Users/kyanchen/Documents/vis_levirship/ab_yolov3'
 	arrow_folder = '/Users/kyanchen/Documents/vis_levirship/ab_yolov3_arrow'
-	max_samples_per_shard = 100  # 每个shard最多存储的样本数
-	n_proc = 1  # 多进程处理数据
+	max_samples_per_shard = 100000  # 每个shard最多存储的样本数，最好10W左右
+	n_proc = 8  # 多进程处理数据，8，16都行
 
 	mmengine.mkdir_or_exist(arrow_folder)
 	img_files = glob.glob(f'{img_folder}/*.png')
@@ -40,7 +40,7 @@ if __name__ == '__main__':
 		results = mmengine.track_parallel_progress(convert_data2dict, data_list_shard, nproc=n_proc)
 		# 保存arrow数据集
 		dataset = Dataset.from_list(results)
-		dataset.save_to_disk(f'{arrow_folder}/data_{i_start}_{i_end}', num_proc=1, max_shard_size='2GB')
+		dataset.save_to_disk(f'{arrow_folder}/data_{i_start}_{i_end}', num_proc=1, max_shard_size='4GB')
 		print(f"Save to {arrow_folder}/data_{i_start}_{i_end}")
 
 
